d906399f5a7de13e1e0be58909065f25641a0e6e
Dan Gyi <daniel.gyi@gmail.com>
2019-08-21 11:39:55 +0100

    first commit - class plus test

---
+++ practicals/object_oriented_design/secret_diary.rb
@@ -0,0 +1,52 @@
+class SecretDiary
+
+  def lock
+    p "Enter Password:"
+    password = gets.chomp
+    if password == '9898'
+      $unlocked = false
+      "Locked!"
+    else
+      "Error, incorrect password"
+    end
+  end
+
+  def unlock
+    p "Enter Password:"
+    password = gets.chomp
+    if password == '9898'
+      $unlocked = true
+      "Unlocked!"
+    else
+      "Error, incorrect password"
+    end
+  end
+
+  def add_entries
+    if $unlocked == true
+      p "Please write your entry"
+      input = gets.chomp
+      file = File.open('diary.csv', "w")
+      csv_line = input + ","
+      file.puts csv_line
+      "Entry saved!"
+    else
+      "Diary locked!"
+    end
+  end
+
+  def get_entries
+    if $unlocked == true
+      p "Getting diary entries"
+      file = File.open('diary.csv', "r")
+
+      file.readlines.each do |row|
+        row.chomp.split(',')
+      end
+    else
+      "Diary locked!"
+    end
+  end
+
+
+end
